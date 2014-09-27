94f425a75260be373ea37432faec0fee670e6493
Brad Stalcup <bstalcup@nd.edu>
2014-09-27 18:04:39 -0400

    modal queue finished

--- public/js/main.js
+++ public/js/main.js
@@ -7,6 +7,41 @@ info['order'] = []
 //busy meter stuff
 
 
+function updateQueueModal() {
+	var modal = $('#queueModal')
+		var Transaction = Parse.Object.extend('Transaction');
+		var query = new Parse.Query(Transaction);
+		query.lessThan("status",2);
+		query.include("item");
+		query.include("order");
+		query.find({
+			success: function(results){
+				modal.find('.row').empty()
+				var string = ""
+				for (var i = 0; i < results.length; i++) {
+					// console.log(results[i].attributes)
+					// console.log(results[i].attributes.item.attributes)
+					// console.log(results[i].attributes.order.attributes)
+					string += '<div class="small-4 columns"><p>Orderer:&nbsp;'+results[i].attributes.order.attributes.name+'</p></div>'
+					string += '<div class="small-4 columns"><p>Item:&nbsp;'+results[i].attributes.item.attributes.name+'</p></div>'
+					string += '<div class="small-4 columns">'
+					if (results[i].attributes.status == 0) {
+						string += '<div class="alert-box secondary">We haven\'t started making it yet</div></div>'
+					}
+					else {
+						string += '<div class="alert-box">We\'re making this item right now!</div></div>' 
+					}
+				};
+				console.log(string);
+				modal.find('.row').append(string);
+			},
+			error: function(mystery, error){
+
+			}
+		});
+
+}
+
 function setClientBusyMeter() {
     var meterLength = 1;
     var numOpenTransactions = 0;
@@ -129,22 +164,12 @@ $( function() {
 	}
 
 	$('#queueButton').click(function(){
-		console.log('8j');
-		var modal = $('#queueModal')
-		var Transaction = Parse.Object.extend('Transaction');
-		var query = new Parse.Query(Transaction);
-		query.greaterThan("status",0);
-		query.find({
-			success: function(){
-				
-			},
-			error: function(mystery, error){
-
-			}
-		})
-		modal.empty()
-		modal.append
+		updateQueueModal();
+		// modal.empty()
+		// modal.append
 	});
+
+
 
 	// init Isotope
 	// var $container = $('.isotope').isotope({
@@ -201,6 +226,7 @@ $( function() {
 
 	setClientBusyMeter();
 	setInterval( "setClientBusyMeter()", 10000 );
+	setInterval( "updateQueueModal()", 10000 );
 
 	$('#checkout').click(function(){
 		var modal = $('#orderModal')
